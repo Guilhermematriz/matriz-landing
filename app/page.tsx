a0f981828f0332604edb66a3e35aa63d0282b0eb
Guilhermematriz <guilhermelourencobroker@gmail.com>
2025-10-24 20:07:09 -0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,2 @@
 import ImobiliariaMatrizLanding_v3 from "../components/ImobiliariaMatrizLanding_v3";
-export default function Page() {
-  return <ImobiliariaMatrizLanding_v3 />;
-}
+export default function Page() { return <ImobiliariaMatrizLanding_v3 />; }
